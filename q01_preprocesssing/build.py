7dcc92733557ba6c950c3dc947f5ddd40c0dd307
greyatomtestuser2 <greyatomtestuser2@gmail.com>
2018-06-19 16:50:23 +0000

    Done

--- q01_preprocesssing/build.py
+++ q01_preprocesssing/build.py
@@ -1,9 +1,21 @@
+# %load q01_preprocesssing/build.py
 import pandas as pd
 import numpy as np
+import datetime
 
+train_df = pd.read_csv('data/train.csv')
 
-train_df = pd.read_csv("data/train.csv")
+def q01_preprocesssing(train_df):
+    'write your solution here'
+    #train_df['Date'] = train_df['Date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+    train_df['Year'] = pd.DatetimeIndex(train_df['Date']).year
+    train_df['Month'] = pd.DatetimeIndex(train_df['Date']).month
+    train_df['Day'] = pd.DatetimeIndex(train_df['Date']).day
+    train_df.set_index('Date')
+    train_df.drop('Date',axis=1,inplace=True)
+    train_df['Store']=train_df['Store'].astype('object')
+    train_df['Dept']=train_df['Dept'].astype('object')
+    return train_df
 
-def q01_preprocesssing():
-    "write your solution here"
+q01_preprocesssing(train_df)
 
