d5546a55bb96f085cffaf766f374c472a7adde60
Alex Anthony <alex19427@gmail.com>
2018-06-21 11:03:41 +0530

    Update build.py

--- q03_autocorrelation_plot/build.py
+++ q03_autocorrelation_plot/build.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sys,os
 sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from pandas.plotting import autocorrelation_plot
-from q01_preprocesssing.build import q01_preprocesssing
+from greyatomlib.walmart_project.q01_preprocesssing.build import q01_preprocesssing
 
 train_df = pd.read_csv("data/train.csv")
 df = q01_preprocesssing(train_df)
